78680b173be6c1e2283f17bccec6a5867fe3a2a1
saruta-hayaet <sarumilk0301@gmail.com>
2023-11-10 08:41:49 +0900

    追加

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -8,6 +8,11 @@
     <div class="main">
         <a href="{{route('project.create')}}"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">案件追加</a>
+            @if (session('alert'))
+            <div class="alert alert-warning">
+                {{ session('alert') }}
+            </div>
+        @endif
         <div class="project-table">
             <div class="th">
                 <div class="project-th">
